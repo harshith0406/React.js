878147bb9e5b0eac76225c8471f132497f64dd0c
Harshith R <harshijeevi2004@gmail.com>
2024-06-19 12:01:22 +0530

    ep1

---
+++ ep1/app.js
@@ -0,0 +1,28 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello world from react"
+);
+
+// const root = ReactDOM.createRoot(document.getElementById("root")); //object
+
+// root.render(heading);
+
+/* <div>
+  <div>
+  <h1>Hello all</h1>
+  </div>  
+</div> */
+// react element which is a object is converted to a HTML which the browser will understand and render.
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "children" },
+    [React.createElement("h1", {}, "parent1"),React.createElement("h1", {}, "parent2")]
+  )
+);
+const root = ReactDOM.createRoot(document.getElementById("root")); //object
+
+root.render(parent);
