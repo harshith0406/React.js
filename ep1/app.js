f7a698ba6b8b35b5d1e4c066e9ed486826b1cb5b
Harshith R <harshijeevi2004@gmail.com>
2024-06-20 22:54:21 +0530

    ep3 done

--- ep1/app.js
+++ ep1/app.js
@@ -25,6 +25,33 @@ const parent = React.createElement(
     [React.createElement("h1", {}, "parent1"),React.createElement("h1", {}, "parent2")]
   )
 );
-const root = ReactDOM.createRoot(document.getElementById("root")); //object
+//this code is transpiled before even the thing is passed to the browser and its done by parcel--babel
 
-root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById("root")); //object
+//react element
+const jsx = <h1>Namaste react using JSX</h1>
+
+//jsx is not html it has like html syntax
+//jsx converted to the react object and then the react object is then compiled to html
+
+
+//react functional component
+const HeadingC = () =>
+  <>
+  <h1>Heading {number}</h1>;
+  {jsx}
+ </>
+
+const HeadingComp = () => {
+  <h1>Heading</h1>;
+};
+const number = 1000;
+const Hello = () => (<h1>Hello</h1>);
+
+
+//root.render(jsx);->conventional way of rendering the react elements
+
+root.render(<HeadingC />)
+//this is way to render all the react functional components
+
+//component composition is nothing but a component inside component
